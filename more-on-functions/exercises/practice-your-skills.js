ae32883254e03687c117d2aa8603d909a0b37856
James Humphrey <james.humprhey@gmail.com>
2024-07-01 16:33:59 -0500

    completed exercises

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -13,3 +13,57 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let myFunction = function(str) {
+    let lettersArray = str.split('');
+    let reversedLettersArray = lettersArray.reverse();
+    return reversedLettersArray.join('');
+ };
+
+console.log("Reverse 'I am Groot.' " + myFunction("I am Groot."));
+
+let nums = [3.14, 42, 4811];
+
+// TODO: Write a mapping function
+// and pass it to .map()
+let halved = nums.map(function(num) {
+    return num/2.0;
+});
+
+console.log(halved);
+
+let names = ["Chris", "Jim", "Sally", "Blake", "Paul"];
+
+// TODO: Write a mapping function
+// and pass it to .map()
+let firstInitials = names.map(function(name) {
+    return name.slice(0,1);
+});
+
+console.log(firstInitials);
+
+function decreasingSum(integer) {
+    if (integer === 1){
+       return integer;
+    } else {
+       let nextSum = decreasingSum(integer-1);
+        console.log(integer + ", " + nextSum);
+        return integer + nextSum;
+    }
+ }
+
+ let tripleArrr = function (arg) {
+    if (typeof arg === "number") {
+        return arg * 3;
+    } else if (typeof arg === "string" ) {
+        return "ARRR!";
+    } else {
+        return arg;
+    }
+ }
+
+ let myArray1 = ['Elocution', 21, 'Clean teeth', 100];
+
+ console.log(myArray1.map(tripleArrr));
+
+ console.log("Decreasing sum of 5: " + decreasingSum(5));
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -29,16 +29,56 @@ console.log("Hold status: " + holdStatus(cargoHold));
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
+let testFuel = function(level) {
+  let minFuelNeeded = 100001;
+  let fuelStatusColor = checkFuel(level);
+  if (fuelStatusColor === "green") {
+    minFuelNeeded = 100001;
+  } else if (fuelStatusColor === "yellow") {
+    minFuelNeeded = 50001;
+  } else if (fuelStatusColor === "red") {
+    minFuelNeeded = 1;
+  } else {
+    minFuelNeeded = level;
+  }
+  fuelLevel = minFuelNeeded;
+  return level - minFuelNeeded;
+}
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
 //c). Once you figure out how much fuel to pump out, return that value.
 
 //d). Decide where to best place your function call to gather our new fuel.
-
+console.log("System Check Follows:");
+console.log("Fuel Level Before Test: " + fuelLevel);
+let fuelIntegrity = testFuel(fuelLevel);
+console.log("Check Fuel: " + fuelIntegrity);
+console.log("Fuel Level After Test: " + fuelLevel);
+console.log("Check Fuel: " + checkFuel(fuelLevel));
 /* Next, liberate some of that glorious cargo.
  */
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
+let cleanHold = function(hold) {
+  let itemsCleaned = [];
+  for (let i = 0; i < hold.length; i++){
+    if (hold[i] === "gold") {
+      itemsCleaned.push(hold[i]);
+      hold[i] = "pyrite";
+    } else if (hold[i] === "space suits") {
+      itemsCleaned.push(hold[i]);
+      hold[i] = "footy pajamas";
+    }
+   }
+  return itemsCleaned;
+}
+
+console.log("Hold Before Cleaning: " + cargoHold);
+console.log("Hold Status: " + holdStatus(cargoHold));
+let holdItemsCleaned = cleanHold(cargoHold);
+console.log("Hold Items Cleaned: " + holdItemsCleaned);
+console.log("Hold After Cleaning: " + cargoHold);
+console.log("Hold Status: " + holdStatus(cargoHold));
 
 //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
 
@@ -54,3 +94,8 @@ console.log("Hold status: " + holdStatus(cargoHold));
 //b). Call your anonymous fuel and cargo functions from within irs.
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
+let irs = function(fuelIntegrity, itemsCleaned) {
+  return `Raided ${fuelIntegrity} of fuel from the tanks, and stole ${itemsCleaned[0]} and ${itemsCleaned[1]} from the cargo hold.`;
+}
+
+console.log(irs(fuelIntegrity, holdItemsCleaned));
