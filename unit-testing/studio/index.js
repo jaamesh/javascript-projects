8321d8961ab02204f56a4723ae3d781fa77ad5d7
James Humphrey <james.humprhey@gmail.com>
2024-07-15 20:25:42 -0500

    working tests and functions

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,73 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 95,
+  programsOffered: ["PT Web Dev", "PT Data Analysis", "FT Data Analysis"],
+  launchOutput: launchOutput
+}
+
+function findFactors(numToFactor) {
+    let factors = [];
+    for(let i = 1; i <= numToFactor; i++) {
+       if(numToFactor % i == 0) {
+            factors.push(i);
+        }
+    }
+    return factors;
+}
+
+function launchOutput(numForCommand) {
+    let factors = findFactors(numForCommand);
+
+    if (factors.includes(2) && !factors.includes(3) && !factors.includes(5)) {
+        return "Launch!";
+    } else if (!factors.includes(2) && factors.includes(3) && !factors.includes(5)) {
+        return "Code!";
+    } else if (!factors.includes(2) && !factors.includes(3) && factors.includes(5)) {
+        return "Rocks!";
+    } else if (factors.includes(2) && factors.includes(3) && !factors.includes(5)) {
+        return "LaunchCode!";
+    } else if (!factors.includes(2) && factors.includes(3) && factors.includes(5)) {
+        return "Code Rocks!";
+    } else if (factors.includes(2) && !factors.includes(3) && factors.includes(5)) {
+        return "Launch Rocks!";
+    } else if (factors.includes(2) && factors.includes(3) && factors.includes(5)) {
+        return "LaunchCode Rocks!";
+    } else if (!factors.includes(2) && !factors.includes(3) && !factors.includes(5)) {
+        return "Rutabagas! That doesn't work.";
+    } else {
+        return "";
+    } 
+    /*
+    if (factors.length === 2) {
+        if (factors[1] === 2) {
+            return "Launch!";
+        } else if (factors[1] === 3) {
+            return "Code!";
+        } else if (factors[1] === 5) {
+            return "Rocks!";
+        }
+    }
+    else if (factors.length === 4) {
+        if (factors[1] === 2 && factors[2] === 3) {
+            return "LaunchCode!";
+        } else if (factors[1] === 3 && factors[2] === 5) {
+            return "Code Rocks!";
+        } else if (factors[1] === 2 && factors[2] === 5) {
+            return "Launch Rocks!";
+        }
+    }
+    else if (factors.length > 4) {
+        if (factors[1] === 2 && factors[2] === 3 && factors.includes(5)) {
+            return "LaunchCode Rocks!";
+        }
+    }
+
+    if (!factors.includes(2) && !factors.includes(3) && !factors.includes(5)) {
+        return "Rutabagas! That doesn't work.";
+    }
+*/
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,46 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("organization returns 'nonprofit'", function() {
+		expect(launchcode.organization).toBe("nonprofit");
+	});
+  test("director returns 'Jeff'", function() {
+		expect(launchcode.executiveDirector).toBe("Jeff");
+	});
+  test("% cool employees 95", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(95);
+	});
+  test("programs offered is 3", function() {
+    expect(launchcode.programsOffered.length).toBe(3);
+	});
+  test("first program is 'PT Web Dev'", function() {
+    expect(launchcode.programsOffered.includes("PT Web Dev")).toBe(true);
+	});
+  test("second program is 'PT Data Analysis'", function() {
+    expect(launchcode.programsOffered.includes("PT Data Analysis")).toBe(true);
+	});
+  test("third program is 'FT Data Analysis'", function() {
+    expect(launchcode.programsOffered.includes("FT Data Analysis")).toBe(true);
+	});
+
+
+let testParams = [
+  [4,"When passed a number that is ONLY divisible by 2","Launch!"],
+  [3,"When passed a number that is ONLY divisible by 3","Code!"],
+  [5,"When passed a number that is ONLY divisible by 5","Rocks!"],
+  [6,"When passed a number that is divisible by 2 and 3","LaunchCode!"],
+  [15,"When passed a number that is divisible by 3 and 5","Code Rocks!"],
+  [10,"When passed a number that is divisible by 2 and 5","Launch Rocks!"],
+  [30,"When passed a number that is divisible by 2, 3 and 5","LaunchCode Rocks!"],
+  [7,"When passed a number that is NOT divisible by 2, 3 or 5","Rutabagas! That doesn't work."]
+                ];
+
+  console.log("testParams:" + testParams);
+  for (let i = 0; i < testParams.length; i++) {
+    test(`${testParams[i][1]}, return '${testParams[i][2]}'`, function() {
+      expect(launchcode.launchOutput(testParams[i][0])).toBe(testParams[i][2]);
+    })
+  }    
+
+
 });
